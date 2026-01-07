15203a9975f3876d812053fc247ed730c617dcab
alvinChristianto <alvinchristianto93@gmail.com>
2026-01-07 10:07:15 +0700

    feat: adding notification on beforeValidate to show toast when code is not unique

--- app/Filament/Resources/PartnersCodes/Pages/CreatePartnersCode.php
+++ app/Filament/Resources/PartnersCodes/Pages/CreatePartnersCode.php
@@ -6,10 +6,35 @@ use App\Filament\Resources\PartnersCodes\PartnersCodeResource;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\PartnerCodeAssigned;
+use Filament\Notifications\Notification;
+use App\Traits\CodeCheck;
 
 class CreatePartnersCode extends CreateRecord
 {
+    use CodeCheck;
     protected static string $resource = PartnersCodeResource::class;
+
+    protected function beforeValidate(): void
+    {
+
+        $res = $this->data;
+        $res['unique_code'] = strtoupper($res['unique_code']);    //make sure its capital
+        $codeToCheckForUnique = $res['unique_code'];
+
+        $isCodeExist = $this->isCodeUnique($codeToCheckForUnique);
+        
+        if ($isCodeExist) {
+            $modalDescAction = "Create atau update data gagal karena duplikasi data ({$res['unique_code']}))";
+            Notification::make()
+                ->title($modalDescAction)
+                ->warning()
+                ->duration(5000)
+                ->send();
+            // 2. Hentikan proses agar tidak lanjut ke database
+            $this->halt();
+        }
+
+    }
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
@@ -17,7 +42,7 @@ class CreatePartnersCode extends CreateRecord
         return $data;
     }
 
-     protected function afterCreate(): void
+    protected function afterCreate(): void
     {
         Mail::to($this->record->partner->email)->send(
             new PartnerCodeAssigned($this->record, $this->record->partner)
