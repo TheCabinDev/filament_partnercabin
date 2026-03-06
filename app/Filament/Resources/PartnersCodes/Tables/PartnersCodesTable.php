1eb785408e0894d4341bfad3924b726194c4d513
BillFedro <muhammadbillfedros@gmail.com>
2026-03-06 13:31:57 +0700

    feat: add Excel export feature to PartnersCodes table

--- app/Filament/Resources/PartnersCodes/Tables/PartnersCodesTable.php
+++ app/Filament/Resources/PartnersCodes/Tables/PartnersCodesTable.php
@@ -14,6 +14,9 @@ use Filament\Actions\ViewAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\ImageColumn;
 use Illuminate\Database\Eloquent\Builder;
+use pxlrbt\FilamentExcel\Actions\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Columns\Column;
 
 class PartnersCodesTable
 {
@@ -148,8 +151,30 @@ class PartnersCodesTable
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
+                    ExportAction::make()->exports([
+                        ExcelExport::make()
+                            ->withFilename(date('Y-m-d') . ' - partnerscodes')
+                            ->withColumns([
+                                // Column::make('id')->heading('NMID'),
+                                Column::make('partner.name')->heading('Partner'),
+                                // Column::make('qrcode_image')->heading('QR Code'),
+                                // Column::make('user.name')->heading('Created By'),
+                                Column::make('unique_code')->heading('Unique Code'),
+                                Column::make('fee_percentage')->heading('Fee (%)'),
+                                Column::make('reduction_percentage')->heading('Reduction (%)'),
+                                Column::make('claim_quota')->heading('Claim Quota'),
+                                Column::make('max_claim_per_account')->heading('Max Claim/Account'),
+                                Column::make('use_started_at')->heading('Start Date'),
+                                Column::make('use_expired_at')->heading('Expired Date'),
+                                Column::make('status')->heading('Status'),
+                                Column::make('created_at')->heading('Created At'),
+                                Column::make('updated_at')->heading('Updated At'),
+                            ]),
+                    ])
                 ]),
             ])
-            ->defaultSort('created_at', 'desc');
+            ->defaultSort('created_at', 'desc')
+            ->striped()
+            ->paginated([10, 25, 50, 100]);
     }
 }
