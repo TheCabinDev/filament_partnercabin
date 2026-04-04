a5e67f3b58bca4c791f9f718d4d871ec5b11be65
alvinChristianto <alvinchristianto93@gmail.com>
2026-04-04 13:14:42 +0700

    feat: adding new filter date_range created_at and check_in_time for claimcoderecord

--- app/Filament/Resources/ClaimCodeRecords/Tables/ClaimCodeRecordsTable.php
+++ app/Filament/Resources/ClaimCodeRecords/Tables/ClaimCodeRecordsTable.php
@@ -2,11 +2,15 @@
 
 namespace App\Filament\Resources\ClaimCodeRecords\Tables;
 
+use Carbon\Carbon;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
@@ -81,7 +85,76 @@ class ClaimCodeRecordsTable
                     ->relationship('partnercode', 'unique_code')
                     ->searchable()
                     ->preload(),
-
+                Filter::make('trx_indate_range')
+                    ->label('Periode Check-in')
+                    ->schema([
+                        DatePicker::make('from') // Nama field sebaiknya berbeda dengan nama kolom agar tidak membingungkan
+                            ->label('CI Dari Tanggal')
+                            ->native(false),
+                        DatePicker::make('until')
+                            ->label('Sampai Tanggal')
+                            ->native(false),
+                    ])
+                    ->columns(2)
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            // Filter jika tanggal "Dari" diisi
+                            ->when(
+                                $data['from'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('check_in_time', '>=', $date),
+                            )
+                            // Filter jika tanggal "Sampai" diisi
+                            ->when(
+                                $data['until'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('check_in_time', '<=', $date),
+                            );
+                    })
+                    // Menampilkan indikator filter aktif di atas tabel (Opsional tapi sangat disarankan)
+                    ->indicateUsing(function (array $data): array {
+                        $indicators = [];
+                        if ($data['from'] ?? null) {
+                            $indicators[] = 'Check-in dari: ' . Carbon::parse($data['from'])->format('d M Y');
+                        }
+                        if ($data['until'] ?? null) {
+                            $indicators[] = 'Check-in sampai: ' . Carbon::parse($data['until'])->format('d M Y');
+                        }
+                        return $indicators;
+                    }),
+                Filter::make('trx_date_range')
+                    ->label('Periode Trx dibuat')
+                    ->schema([
+                        DatePicker::make('trx_from') // Nama field sebaiknya berbeda dengan nama kolom agar tidak membingungkan
+                            ->label('Trx Dari Tanggal')
+                            ->native(false),
+                        DatePicker::make('trx_until')
+                            ->label('Sampai Tanggal')
+                            ->native(false),
+                    ])
+                    ->columns(2)
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            // Filter jika tanggal "Dari" diisi
+                            ->when(
+                                $data['trx_from'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            // Filter jika tanggal "Sampai" diisi
+                            ->when(
+                                $data['trx_until'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    })
+                    // Menampilkan indikator filter aktif di atas tabel (Opsional tapi sangat disarankan)
+                    ->indicateUsing(function (array $data): array {
+                        $indicators = [];
+                        if ($data['from'] ?? null) {
+                            $indicators[] = 'Dibuat dari: ' . Carbon::parse($data['trx_from'])->format('d M Y');
+                        }
+                        if ($data['until'] ?? null) {
+                            $indicators[] = 'Dibuat sampai: ' . Carbon::parse($data['trx_until'])->format('d M Y');
+                        }
+                        return $indicators;
+                    })
 
 
             ])
